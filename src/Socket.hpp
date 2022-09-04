fee725a10a5d418fd34e910a5b6299ad70938222
Daniel Lizano <dlizano@outlook.com>
2022-09-03 22:37:49 -0600

    se agrega coidgo de repo anterior por porblemas de maquina

---
+++ src/Socket.hpp
@@ -0,0 +1,60 @@
+/// @copyright 2022 ECCI, Universidad de Costa Rica. All rights reserved
+/// @author Michelle Fonseca Carrillo <michelle.fonsecacarrillo@ucr.ac.cr>
+/// @author Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
+/// @author Daniel Lizano Morales <daniel.lizanomorales@ucr.ac.cr>
+/// @author Jorge Loría López <jorge.lorialopez@ucr.ac.cr>
+/// This code is released under the GNU Public License version 3
+
+#ifndef SOCKET_HPP
+#define SOCKET_HPP
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+//#include <openssl/ssl.h>
+//#include <openssl/err.h>
+
+#include <iostream>
+
+#define DEFAULT_PORT 8080;
+
+class Socket {
+ private:
+  int id;
+  int port;
+  bool ipv6;
+  void* SSLStruct;
+  void* SSLContext;
+  
+ public:
+  explicit Socket(char, bool = false);
+  explicit Socket(int);
+  ~Socket();
+  int Connect(const char*, int);
+  int Connect(const char*, const char*);
+  int ConnectIPV6(const char*, int);
+  int ConnectIPV6(const char*, const char*);
+  void Close();
+  int Read(char*, int);
+  int Write(const char*, int);
+  int Write(const char*);
+  int Listen(int);
+  int Bind(int);
+  Socket* Accept();
+  int Shutdown(int);
+  void SetId(int);
+  // SSL Methods
+  void InitSSLContext();
+	void InitSSL();
+	int SSLConnect(const char*, int);
+	int SSLConnect(const char*, char*);
+	int SSLRead(void*, int);
+	int SSLWrite(void*, int);
+};
+
+#endif  // !SOCKET_HPP
---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+/// @copyright 2022 ECCI, Universidad de Costa Rica. All rights reserved
+/// @author Michelle Fonseca Carrillo <michelle.fonsecacarrillo@ucr.ac.cr>
+/// @author Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
+/// @author Daniel Lizano Morales <daniel.lizanomorales@ucr.ac.cr>
+/// @author Jorge Loría López <jorge.lorialopez@ucr.ac.cr>
+/// This code is released under the GNU Public License version 3
+
+#define DEFAULT
+#define SIZE 1024
+
+#include <stdio.h>
+#include <string.h>
+
+#include "Socket.hpp"
+// #include "DataSource.hpp"
+#include "Round.hpp"
+//#include "JsonDataSource.hpp"
+
+int main(int argc, char * argv[]) {
+  const char* os = "http://os.ecci.ucr.ac.cr/";
+  //const char* osn = "10.1.104.187";  // Public ip
+  const char* osn = "163.178.104.187";  // Private ip  
+  const char* request = "GET /futbol/2018/world-cup-2018.json HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
+  Socket s('s');
+  char a[512];
+  s.Connect(osn, 80);
+  s.Write(request);
+  while (s.Read(a, 512) != 0) {
+    printf("%s\n", a);
+    memset(a, 0, 512);
+  }
+
+  //char * os = (char *) "http://os.ecci.ucr.ac.cr/";
+  //char * reqOS = (char *) "GET /futbol/2018/world-cup-2018.json HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
+  //Socket s( 's', false );	// Create a new stream socket for IPv4
+  //char a[ SIZE ];
+
+  //memset( a, 0, SIZE );
+  //s.InitSSL();
+  //s.SSLConnect( os, (char *) "https" );
+  //s.SSLWrite(  (void *) reqOS, strlen( reqOS ) );
+  //while ( (s.SSLRead( a, SIZE )) > 0 ) {   // Waits until conection close
+  //  printf( "%s\n", a );
+  //  memset( a, 0, SIZE );
+  //}
+  //printf( "%s\n", a );
+}
+
