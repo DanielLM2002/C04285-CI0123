abfcb22dc60bfdd49669103c5b787e44d67244df
DanielLM2002 <dlizano@outlook.com>
2022-09-09 00:44:42 -0600

    se elimina codeigo no necesario

--- Tarea02/src/Socket.hpp
+++ Tarea02/src/Socket.hpp
@@ -1,9 +1,3 @@
-/// @copyright 2022 ECCI, Universidad de Costa Rica. All rights reserved
-/// @author Michelle Fonseca Carrillo <michelle.fonsecacarrillo@ucr.ac.cr>
-/// @author Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
-/// @author Daniel Lizano Morales <daniel.lizanomorales@ucr.ac.cr>
-/// @author Jorge Loría López <jorge.lorialopez@ucr.ac.cr>
-/// This code is released under the GNU Public License version 3
 
 #ifndef _SOCKET_
 #define _SOCKET_
@@ -37,17 +31,10 @@ class Socket {
   void* SSLContext;
   
  public:
-  /**
-   * @brief Constructor
-   * @param type Specifies the communication semantics
-   * @param ipv6 Indicates if ipv6 is used
-   */
+  /// Constructor
   explicit Socket(char, bool = false);
 
-  /**
-   * @brief Construct a new Socket object
-   * @param id Socket id
-   */
+  /// Constructor
   explicit Socket(int);
 
   /**
@@ -55,100 +42,41 @@ class Socket {
    */
   ~Socket();
 
-  /**
-   * @brief Method that connects with an ip address and a port for IPV4
-   * @param host Ip address
-   * @param port Port number
-   * @return int 
-   */
+
   int Connect(const char*, int);
 
-  /**
-   * @brief Method that connects with an ip address using a service for IPV4
-   * @param hostip Ip address
-   * @param service Service that is used
-   * @return int 
-   */
+
   int Connect(const char*, const char*);
 
-  /**
-   * @brief Method that connects with an ip address and a port for IPV6
-   * @param host Ip address
-   * @param port Port number
-   * @return int 
-   */
   int ConnectIPV6(const char*, int);
 
-  /**
-   * @brief Method that connects with an ip address using a service for IPV6
-   * @param host Ip address
-   * @param service Service that is used
-   * @return int 
-   */
+
   int ConnectIPV6(const char*, const char*);
   
-  /**
-   * @brief Method that closes a socket
-   */
+
   void Close();
 
-  /**
-   * @brief Method that reads data from a socket
-   * @param data Data to read
-   * @param size Size of the data vector
-   * @return int 
-   */
+
   int Read(char*, int);
 
-  /**
-   * @brief Method that writes to a socket
-   * @param data Data to write
-   * @return int 
-   */
+
   int Write(const char*);
 
-  /**
-   * @brief Method that inits the SSL properties of the socket
-   */
+
   void InitSSL();
 
-  /**
-   * @brief Method that inits the SSL context of the socket
-   */
   void InitSSLContext();
 
-  /**
-   * @brief Method that connects with an ip address and a port for IPV4 
-   *        using SSL protocol
-   * @param host Ip address
-   * @param port Port number
-   * @return int 
-   */
+
   int SSLConnect(const char*, int);
 
-  /**
-   * @brief Method that connects with an ip address for IPV4 using a service
-   *        and SSL protocol
-   * @param host Ip address
-   * @param service Service that is used
-   * @return int 
-   */
+
   int SSLConnect(const char*, const char*);
 
-  /**
-   * @brief Method that reads data from a socket using SSL protocol
-   * @param data Data to read
-   * @param size Size of the data vector
-   * @return int 
-   */
+
   int SSLRead(const void*, int);
 
-  /**
-   * @brief Method that writes to a socket using SSL protocol
-   * @param data Data to write
-   * @param size Size of the data vector
-   * @return int 
-   */
+
   int SSLWrite(const void*, int);
 };
 
--- Tarea02/src/main.cpp
+++ Tarea02/src/main.cpp
@@ -7,33 +7,29 @@
 
 #include <stdio.h>
 #include <string.h>
-//#include "ssl.h"
-#include "Socket.hpp"
-#include <iostream>
 
+#include "Socket.hpp"
 
 #define SIZE 1024
 
 int main( int argc, char * argv[] ) {
-   char * dirIp = (char *) "163.178.104.187";
    char * os = (char *) "os.ecci.ucr.ac.cr";
-   char * reqOS = (char *) "GET /futbol/2018/clasificacion.txt HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
-   //https://raw.githubusercontent.com/openfootball/worldcup.json/master/2018/worldcup.standings.json
-   Socket s( 's', false ); // Create a new stream socket for IPv4
+   //char * WM = (char *) "10.1.104.187";
+   char * WM = (char *) "163.178.184.187";
+   char * reqOS = (char *) "GET /futbol/2018/index.php?country=FRA HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
+   Socket s( 's', false );	// Create a new stream socket for IPv4
    char a[ SIZE ];
-   std::string carry = "";
+   int count;
 
    memset( a, 0, SIZE );
    s.InitSSL();
-   s.SSLConnect(dirIp, (char *) "https");
+   s.SSLConnect( WM, 80);
    s.SSLWrite(  (void *) reqOS, strlen( reqOS ) );
-   while (  (s.SSLRead( a, SIZE )) > 0 ) {
-      //printf( "%s\n", a );
-      carry += a;
+   while (  (count = s.SSLRead( a, SIZE )) > 0 ) {
+      printf( "%s\n", a );
       memset( a, 0, SIZE );
    }
 
-   std::cout << carry << std::endl;
-   //printf( "%s\n", a );
+   printf( "%s\n", a );
 
 }
