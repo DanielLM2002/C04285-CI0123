3e244618e710f0275cd98109a48fb0345ca458f5
Daniel Lizano <dlizano@outlook.com>
2022-09-08 23:23:05 -0600

    correcicones de ssl

--- Tarea02/src/Socket.hpp
+++ Tarea02/src/Socket.hpp
@@ -1,35 +1,28 @@
-/*
- *  Establece la definicion de la clase Socket para efectuar la comunicacion
- *  de procesos que no comparten memoria, utilizando un esquema de memoria
- *  distribuida.  El desarrollo de la clase se hara en dos etapas, primero
- *  los metodos necesarios para los clientes, en la otra etapa los otros
- *  metodos para los servidores
- */
-
 #ifndef Socket_h
 #define Socket_h
 
 class Socket{
 
-    public:
-        Socket( char, bool = false );
-        Socket( int );
-        ~Socket();
-        void Close();
-        int Connect( const char *, int );
-        void InitSSLContext();
-        void InitSSL();
-        int SSLConnect( char *, int );
-        int SSLConnect( char *, char * );
-        int SSLRead( void *, int );
-        int SSLWrite( void *, int );
-        
-    private:
-        int idSocket;
-        int port;
-        bool ipv6;
-        void * SSLContext;	// SSL context
-	    void * SSLStruct;	// SSL BIO
+   public:
+      // Otros metodos definidos por usted
+
+// SSL methods
+   private:
+      void InitSSLContext();
+   public:
+      void InitSSL();
+      void SSLConnect( char *, int );
+      void SSLConnect( char *, char * );
+      int SSLRead( void *, int );
+      int SSLWrite( const void *, int );
+
+   private:
+      // Otras instancias de la clase definidas por usted
+
+// SSL instance variables
+      void * SSLContext;		// SSL context
+      void * SSLStruct;		// SSL BIO
+
 };
 
 #endif
