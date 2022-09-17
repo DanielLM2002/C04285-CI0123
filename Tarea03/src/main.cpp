1afa90b02fdac821335cd837bff89ff1f93327e7
DanielLM2002 <dlizano@outlook.com>
2022-09-17 01:04:21 -0600

    se agrega main de prueba

--- Tarea03/src/main.cpp
+++ Tarea03/src/main.cpp
@@ -1,20 +1,17 @@
-#define SIZE 1024
-
+/*
+ *
+ */
+#include <stdio.h>
 #include "Socket.hpp"
 
-int main(int argc, char * argv[]) {
-  char* os = (char*) "http://os.ecci.ucr.ac.cr/";
-  const char* osn = "163.178.104.187";  // Private ip 
-  const char* request = "GET /futbol/2018/clasificacion.txt HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
-  Socket s('s', false);	// Create a new stream socket for IPv4
-  char a[SIZE];
-  memset(a, 0, SIZE);
-  s.InitSSL();
-  s.SSLConnect(osn, 443);
-  s.SSLWrite((void*) request, strlen(request));
-  while ((s.SSLRead(a, SIZE)) > 0) {   // Waits until conection close
-   printf("%s\n", a);
-   memset(a, 0, SIZE);
-  }
-  printf("%s\n", a);
+
+int main( int argc, char ** argv ) {
+   Socket s();     // Crea un socket de IPv4, tipo "stream"
+   char buffer[ 512 ];
+
+  s.Connect( "ip address in dot decimal format", 9876 ); // Same port as server
+  s.Write( argv[1] );		// Send first program argument to server
+  s.Read( buffer, 512 );	// Read the answer sent back from server
+   printf( "%s", buffer );	// Print the received string
+
 }
