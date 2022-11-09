68a1d522a97e0f5994dff27d65d6a4d4680acbe4
DanielLM2002 <dlizano@outlook.com>
2022-11-09 02:24:54 -0600

    se arregla main de tarea 2 por el correcto del profesor (url e IP incorrecta)

--- Tarea02/src/main.cpp
+++ Tarea02/src/main.cpp
@@ -10,20 +10,22 @@
 
 #include "Socket.hpp"
 
-#define SIZE 1024
+#define test2
 
-int main( int argc, char * argv[] ) {
-   char * os = (char *) "os.ecci.ucr.ac.cr";
+#define SIZE 1024
+#ifdef test1
+int main (int argc, char * argv[] ) {
+   char * os = (char *) "http://os.ecci.ucr.ac.cr";
    //char * WM = (char *) "10.1.104.187";
-   char * WM = (char *) "163.178.184.187";
-   char * reqOS = (char *) "GET /futbol/2018/index.php?country=FRA HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
+   char * WM = (char *) "163.178.104.187";
+   char * reqOS = (char *) "GET /futbol/2018/clasificacion.txt HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
    Socket s( 's', false );	// Create a new stream socket for IPv4
    char a[ SIZE ];
    int count;
 
    memset( a, 0, SIZE );
    s.InitSSL();
-   s.SSLConnect( WM, 433);
+   s.SSLConnect( WM, 443);
    s.SSLWrite(  (void *) reqOS, strlen( reqOS ) );
    while (  (count = s.SSLRead( a, SIZE )) > 0 ) {
       printf( "%s\n", a );
@@ -33,3 +35,28 @@ int main( int argc, char * argv[] ) {
    printf( "%s\n", a );
 
 }
+#endif
+
+#ifdef test2
+int main( int argc, char * argv[] ) {
+   char * os = (char *) "http://os.ecci.ucr.ac.cr/";
+   //char * WM = (char *) "10.1.104.187";
+   char * WM = (char *) "163.178.104.187";
+   char * reqOS = (char *) "GET /futbol/2018/clasificacion.txt HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
+   Socket s( 's', false );	// Create a new stream socket for IPv4
+   char a[ SIZE ];
+   int count;
+
+   memset( a, 0, SIZE );
+   s.InitSSL();
+   s.SSLConnect( WM, 443);
+   s.SSLWrite(  (void *) reqOS, strlen( reqOS ) );
+   while (  (count = s.SSLRead( a, SIZE )) > 0 ) {
+      printf( "%s\n", a );
+      memset( a, 0, SIZE );
+   }
+
+   printf( "%s\n", a );
+
+}
+#endif
