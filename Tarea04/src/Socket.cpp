43256ceeff89efecb75ef9eb26060d09ec8961c8
Daniel Lizano <dlizano@outlook.com>
2022-11-13 23:50:29 -0600

    Se agerega codigo base de las tareas anteriores

---
+++ Tarea04/src/Socket.cpp
@@ -0,0 +1,173 @@
+
+#include "Socket.hpp"
+
+/**
+ * @brief Socket constructor
+ * @param type Specifies the communication semantics
+ * @param ipv6 Indicates if ipv6 is used
+ */
+Socket::Socket(char type, bool ipv6) {
+  this->ipv6 = ipv6;
+  this->id = -1;
+  this->port = DEFAULT_PORT;
+  this->SSLStruct = nullptr;
+  this->SSLContext = nullptr;
+  if (!this->ipv6)
+    this->id = type == 's' ? socket(AF_INET, SOCK_STREAM, 0)
+                           : socket(AF_INET, SOCK_DGRAM, 0);
+  else
+    this->id = type == 's' ? socket(AF_INET6, SOCK_STREAM, 0)
+                           : socket(AF_INET6, SOCK_DGRAM, 0);
+}
+
+Socket::Socket( int id ) {
+  this->id = id;
+}
+
+/**
+ * @brief Method that closes a socket
+ */
+Socket::~Socket() {
+  this->Close();
+}
+
+void Socket::Close(){
+  close(this->id);
+}
+
+/**
+ * @brief Method that connects with a port and an ip address
+ * @param hostip Ip address
+ * @param port Port number
+ * @return int 
+ */
+int Socket::Connect(const char* hostip, int port) {
+  struct sockaddr_in host4;
+  memset(reinterpret_cast<char *>(&host4), 0, sizeof(host4));
+  host4.sin_family = AF_INET;
+  inet_pton(AF_INET, hostip, &host4.sin_addr);
+  host4.sin_port = htons(port);
+  int st = connect(this->id, reinterpret_cast<sockaddr *>(&host4),
+    sizeof(host4));
+  if (st == -1) {
+    perror("Socket::Connect");
+    exit(2);
+  }
+  return st;
+}
+
+/**
+ * @brief Method that connects with a port and an ip address
+ * @param hostip Ip address
+ * @param port Port address
+ * @return int 
+ */
+int Socket::Connect(const char* hostip, const char* port) {
+  struct sockaddr_in host4;
+  memset(reinterpret_cast<char *>(&host4), 0, sizeof(host4));
+  host4.sin_family = AF_INET;
+  inet_pton(AF_INET, hostip, &host4.sin_addr);
+  host4.sin_port = htons(atoi(port));
+  int st = connect(this->id, reinterpret_cast<sockaddr *>(&host4),
+           sizeof(host4));
+  if (st == -1) {
+    perror("Socket::Connect");
+    exit(2);
+  }
+  return st;
+}
+
+/**
+ * @brief Method that connects with a port and an ip address
+ * in IPV6
+ * @param hostip Ip address
+ * @param port Port address
+ * @return int 
+ */
+int Socket::ConnectIPV6(const char* hostip, int port) {
+  struct sockaddr_in6 host6;
+  memset(reinterpret_cast<char *>(&host6), 0, sizeof(host6));
+  host6.sin6_family = AF_INET6;
+  inet_pton(AF_INET6, hostip, &host6.sin6_addr);
+  host6.sin6_port = htons(port);
+  int st = connect(this->id, reinterpret_cast<sockaddr *>(&host6),
+           sizeof(host6));
+  if (st == -1) {
+    perror("Socket::Connect");
+    exit(2);
+  }
+  return st;
+}
+
+/**
+ * @brief Method that connects with a port and an ip address
+ * in IPV6
+ * @param hostip Ip address
+ * @param port Port address
+ * @return int 
+ */
+int Socket::ConnectIPV6(const char* hostip, const char* port) {
+  struct sockaddr_in6 host6;
+  memset(reinterpret_cast<char *>(&host6), 0, sizeof(host6));
+  host6.sin6_family = AF_INET6;
+  inet_pton(AF_INET6, hostip, &host6.sin6_addr);
+  host6.sin6_port = htons(atoi(port));
+  int st = connect(this->id, reinterpret_cast<sockaddr *>(&host6),
+           sizeof(host6));
+  if (st == -1) {
+    perror("Socket::Connect");
+    exit(2);
+  }
+  return st;
+}
+
+/**
+ * @brief Method that reads from a socket
+ * @param info Information to read
+ * @param length Size of the information vector
+ * @return int 
+ */
+int Socket::Read(char* info, int length) {
+  return read(this->id, info, length);
+}
+
+/**
+ * @brief Method that writes to a socket
+ * @param info Information to write
+ * @return int 
+ */
+int Socket::Write(const char* info) {
+  return write(this->id, info, strlen(info));
+}
+
+int Socket::sendTo(const void *, int, void *) {
+  struct sockaddr_in host4;
+  memset(reinterpret_cast<char *>(&host4), 0, sizeof(host4));
+  host4.sin_family = AF_INET;
+  host4.sin_port = htons(this->port);
+  host4.sin_addr.s_addr = htonl(INADDR_ANY);
+  int st = sendto(this->id, reinterpret_cast<sockaddr *>(&host4),
+           sizeof(host4), 0, reinterpret_cast<sockaddr *>(&host4),
+           sizeof(host4));
+  if (st == -1) {
+    perror("Socket::sendTo");
+    exit(2);
+  }
+  return st;
+}
+
+int Socket::recvFrom(void *, int, void *) {
+  struct sockaddr_in host4;
+  memset(reinterpret_cast<char *>(&host4), 0, sizeof(host4));
+  host4.sin_family = AF_INET;
+  host4.sin_port = htons(this->port);
+  host4.sin_addr.s_addr = htonl(INADDR_ANY);
+  int st = recvfrom(this->id, reinterpret_cast<sockaddr *>(&host4),
+           sizeof(host4), 0, reinterpret_cast<sockaddr *>(&host4),
+           sizeof(host4));
+  if (st == -1) {
+    perror("Socket::recvFrom");
+    exit(2);
+  }
+  return st;
+}
--- Tarea04/src/Socket.hpp
+++ Tarea04/src/Socket.hpp
@@ -1,32 +1,56 @@
 
+#ifndef SOCKET_HPP
+#define SOCKET_HPP
 
-#ifndef Socket_h
-#define Socket_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+//#include <openssl/ssl.h>
+//#include <openssl/err.h>
 
-class Socket{
+#include <iostream>
 
-    public:
-        Socket( char, bool = false );
-        Socket( int );
-        ~Socket();
-        int Connect( char *, int );
-        int Connect( char *, char * );
-        void Close();
-        int Read( char *, int );
-        int Write( char *, int );
-        int Write( char * );
-        int Listen( int );
-        int Bind( int );
-        Socket * Accept();
-        int Shutdown( int );
-        void SetIDSocket( int );
-        int sendTo( const void *, int, void * );
-        int recvFrom( void *, int, void * );
+#define DEFAULT_PORT 8080;
 
-    private:
-        int idSocket;
-        bool ipv6;
-        int port;
+class Socket {
+ private:
+  int id;
+  int port;
+  bool ipv6;
+  void* SSLStruct;
+  void* SSLContext;
+  
+ public:
+  explicit Socket(char, bool = false);
+  explicit Socket(int);
+  ~Socket();
+  int Connect(const char*, int);
+  int Connect(const char*, const char*);
+  int ConnectIPV6(const char*, int);
+  int ConnectIPV6(const char*, const char*);
+  void Close();
+  int Read(char*, int);
+  int Write(const char*, int);
+  int Write(const char*);
+  int Listen(int);
+  int Bind(int);
+  Socket* Accept();
+  int Shutdown(int);
+  void SetId(int);
+  int sendTo( const void *, int, void * );
+  int recvFrom( void *, int, void * );
+  // SSL Methods
+  void InitSSLContext();
+	void InitSSL();
+	int SSLConnect(const char*, int);
+	int SSLConnect(const char*, char*);
+	int SSLRead(void*, int);
+	int SSLWrite(void*, int);
 };
 
-#endif
+#endif  // !SOCKET_HPP
